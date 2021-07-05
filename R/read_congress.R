a501fd351e7c93e6e33363365c1744ac9cffac70
Joyce Robbins <jtr13@columbia.edu>
2021-07-05 16:41:51 -0400

    add back Independents

--- R/read_congress.R
+++ R/read_congress.R
@@ -1,12 +1,12 @@
 # Script to clean congress data
 
-readr::read_csv("https://theunitedstates.io/congress-legislators/legislators-current.csv") %>% 
-  select(full_name, gender, type, party, birthday) %>% 
-  mutate(gender = fct_relevel(gender, "M"),
-         type = fct_recode(type, House = "rep", Senate = "sen"),
-         party = fct_recode(party, Dem = "Democrat", Rep = "Republican", Ind = "Independent"),
-         yrs = as.integer(floor((today()-birthday)/365.25)),
-         age = paste0(floor(yrs/10), "0s")) %>% 
-  filter(party != "Ind") %>% 
-  mutate(party = factor(party)) %>% # drops unused levels
-  write_csv("data/congress.csv")
+library(magrittr)
+readr::read_csv("https://theunitedstates.io/congress-legislators/legislators-current.csv") %>%
+  dplyr::select(full_name, gender, type, party, birthday) %>%
+  dplyr::mutate(gender = forcats::fct_relevel(gender, "M"),
+         type = forcats::fct_recode(type, House = "rep", Senate = "sen"),
+         party = forcats::fct_recode(party, Dem = "Democrat", Rep = "Republican", Ind = "Independent"),
+         yrs = as.integer(floor((Sys.Date()-birthday)/365.25)),
+         age = paste0(floor(yrs/10), "0s")) %>%
+  dplyr::mutate(party = factor(party)) %>% # drops unused levels
+  readr::write_csv("data/congress.csv")
