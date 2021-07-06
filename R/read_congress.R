9b8ae98bb6b97d655e23f28e5cbb99afb3bcd8d6
Joyce Robbins <jtr13@columbia.edu>
2021-07-06 19:18:06 -0400

    remove relevel

--- R/read_congress.R
+++ R/read_congress.R
@@ -3,8 +3,7 @@
 library(magrittr)
 readr::read_csv("https://theunitedstates.io/congress-legislators/legislators-current.csv") %>%
   dplyr::select(full_name, gender, type, party, birthday) %>%
-  dplyr::mutate(gender = forcats::fct_relevel(gender, "M"),
-         type = forcats::fct_recode(type, House = "rep", Senate = "sen"),
+  dplyr::mutate(type = forcats::fct_recode(type, House = "rep", Senate = "sen"),
          party = forcats::fct_recode(party, Dem = "Democrat", Rep = "Republican", Ind = "Independent"),
          yrs = as.integer(floor((Sys.Date()-birthday)/365.25)),
          age = paste0(floor(yrs/10), "0s")) %>%
